86e8dbba8f2d966ab32d9838f30bc23f42fcda24
shima-529 <simatyan@gmail.com>
2018-10-24 01:48:46 +0900

    [FIX] operation of progress bar on (filesize % 256) != 0
    (fileSize % 256) != 0 の時のプログレスバーの挙動を修正

--- progress.cpp
+++ progress.cpp
@@ -2,14 +2,14 @@
 namespace Progress {
 	constexpr int width = 40;
 	void show(char *prefix, char *postfix, uint32_t val, uint32_t max) {
-		int outputLen = (val + 0.0) / max * width;
+		uint32_t outputLen = (val + 0.0) / max * width;
 
 		std::cout << prefix << " ";
 		std::cout << "[";
-		for(int i=0; i<outputLen; i++){
+		for(uint32_t i=0; i <= outputLen; i++){
 			std::cout << "=";
 		}
-		for(int i=0; i<width-outputLen; i++){
+		for(uint32_t i=0; i < width-outputLen; i++){
 			std::cout << " ";
 		}
 		std::cout << "] " << postfix << "\r";
