6352bd13ba080e70579ebe73736b0e5740bc9203
shima-529 <simatyan@gmail.com>
2018-10-24 01:46:11 +0900

    [ADD] for data read
    データ読み出し用に追加

--- argInfo.cpp
+++ argInfo.cpp
@@ -2,6 +2,7 @@
 
 namespace ArgInfo {
 	char *binPath;
+	char *sizeStrForRead;
 	int portFd;
 	uint32_t baud = 115200;
 	Operation op;
--- argInfo.hpp
+++ argInfo.hpp
@@ -1,10 +1,11 @@
 #include <cstdint>
 namespace ArgInfo {
 	extern char *binPath;
+	extern char *sizeStrForRead;
 	extern int portFd;
 	extern uint32_t baud;
 	enum struct Operation {
-		Write, Monitor,
+		Write, Read, Monitor,
 	};
 	extern Operation op;
 } // namespace ArgInfo
